a49ad0d1b342a196000d158151b7636f92aa4f37
prabuddha chunchekar <prabuddhachunchekar@gmail.com>
2025-04-03 00:43:23 +0530

    lenis added

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// Initialize Lenis
+const lenis = new Lenis();
 
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
   
 
 
